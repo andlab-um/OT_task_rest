c7b875206df856d7b69c427214977969fa1619ce
qingyuan6529 <73586593+qingyuan6529@users.noreply.github.com>
2022-07-15 11:07:17 +0800

    Update oldreconstract.m

--- oldreconstract.m
+++ oldreconstract.m
@@ -1,49 +1,59 @@
-%OTtoOT
+% The inputs of this script include all resting and task ICs obtained by GIFT, and the aggbeta.mat(the outputs of script”1_OTaggbeta”).
+% Three main outputs will be obtained by this script:
+% 1. menold.mat is a 30*30 (in the OT group) or 29*29(in the PL group) correlation matrix, because of the resting data of every subject are combined with his own models to obtain the predicted activation of all 30(or 29) subjects. 
+% 2. predict.mat stores the self-predicted task activation. The predicted task activation of each subject are calculated using his own resting data and model. predict.mat is a 57549 (voxels) * 30(subjects in OT group) or a 57549 (voxels) * 29(subjects in PL group) matrix for drawing the predicted task brain activation map. 
+% 3. percentageold.mat showed the increasing degree of self-predicted accuracy relative to other-predicted accuracy. It is calculated by subtracting averaged other-predicted accuracy from the self-predicted accuracy and then calculating the percentage increase over the averaged other-predicted accuracy. This process is performed in each subject, thus percentage.mat is a 30 (subjects in OT group) * 1 or a 29 (subjects in PL group) * 1 matrix.
+
+% Due to there are 2 sessions, the first loop need to run twice for every task IC.
+
 rz=[];
 pz=[];
 %predict=[];
-for i=1:29
-    r=[];
-    p=[];
-    %提取每个被试的静息态ic
-    namerest = strcat('PLot_ica_br',int2str(i),'.mat');
-    load(['E:\data\OT\OT_Resting\ICA_results_2\PL\',namerest]);
-    b=compSet.ic;
-%     PL静息态成分及最后的X
-      %delete=[2,9,14,27,28];
-      delete=[1,2,3,5,7,9,10,11,12,14,18,19,20,23,25,26,27,28,31,32,34];
-      b(delete,:)=[];
-      XrestPL=b';
-      
-% %     OT静息态成分及最后的X
-%       delete=[8,9,12,15,23,33];
-%       b(delete,:)=[];
-%       XrestOT=b';
-    
+for j=1:2
+    for i=1:29
+        r=[];
+        p=[];
+        %     resting ICs for each subject
+        namerest = strcat('PLot_ica_br',int2str(i),'.mat');
+        load(['E:\data\OT\OT_Resting\ICA_results_2\PL\',namerest]);
+        b=compSet.ic;
+
+        delete=[1,2,3,5,7,9,10,11,12,14,18,19,20,23,25,26,27,28,31,32,34];
+        b(delete,:)=[];
+        XrestPL=b';
+     
         for c=1:29
-        t=(2:2:58);
-        e=t(1,c);
-        namefunc = strcat('PLot_ica_br',int2str(e),'.mat');
-        load(['E:\data\OT\OT_Functional\ICA\new\testPL_OTdefaultmask\',namefunc]) ;%使用任务态默认生成mask的被试的ica数据
-        yorigin=(compSet.ic(27,:))';
-        ypredict=XrestPL*agg_beta27(:,i);%与myreconstruct不同的是，这里使用的是第i个被试的静息态数据和第i个被试的30个主成分的betamap
-        %把自己预测自己得出的y的提取出来
-%         tf=isequal(1,c);
-%         if tf>0
-%             predict=[predict,ypredict];
-%         else
-%             predict=predict;
-%         end
-        
-        [rho,pval] = corr(ypredict,yorigin);
-        r=[r,rho];
-        p=[p,pval];
-        end
-    rz=[rz;r];
-    pz=[pz;p];
+            t=(j:2:58);
+            e=t(1,c);
+            namefunc = strcat('PLot_ica_br',int2str(e),'.mat');
+            load(['E:\data\OT\OT_Functional\ICA\new\testPL_OTdefaultmask\',namefunc]) ;
+            
+            % detailed instruction see script "3.my reconstruct"
+            yorigin=(compSet.ic(27,:))';
+            
+            if j<2
+                ypredict=XrestPL*agg_beta27(:,i);
+            else
+                ypredict=XrestPL*agg_beta27(:,i+29);
+            end
+            %把自己预测自己得出的y的提取出来
+    %         tf=isequal(1,c);
+    %         if tf>0
+    %             predict=[predict,ypredict];
+    %         else
+    %             predict=predict;
+    %         end 
     
+% Unlike the "3.my reconstruct", here using the resting state data of subject i and the betamap of the 30 principal components of subject i
+            [rho,pval] = corr(ypredict,yorigin);
+            r=[r,rho];
+            p=[p,pval];
+        end
+        rz=[rz;r];
+        pz=[pz;p];
+    end     
 end
-%把每个被试两个session的相关系数平均起来,第一行是第一个人预测所有人，第二行是第二个人预测所有人，以此类推。
+
 menold=[];
 for g=1:29
     h=g+29;
@@ -52,7 +62,7 @@ for g=1:29
 end
 save('E:\data\OT\predictresult\newstandard\PLtoPL\28DDMN\menold.mat','menold');
 %save('E:\data\OT\predictresult\newstandard\OTtoOT1\26VDMN\predict.mat','predict');
-%算自己预测自己和自己预测他人之间的差
+
 differenceold=[];
 precentageold=[];
 for i=1:29
@@ -67,7 +77,6 @@ end
 save('E:\data\OT\predictresult\newstandard\PLtoPL\28DDMN\differenceold.mat','differenceold');
 save('E:\data\OT\predictresult\newstandard\PLtoPL\28DDMN\precentageold.mat','precentageold');
 
-%将预测自己和预测他人的均值都提取出来
 predictselfold=[];
 predictotherold=[];
 for i=1:30
